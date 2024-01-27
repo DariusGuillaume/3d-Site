2f3008351f2b7cfe1f14ae4cc947fde8555cc182
DariusGuillaume <dariusguillaume2@gmail.com>
2024-01-27 18:52:46 -0500

    Added white logo, red letters in navbar and scene is at bottom after a scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,38 @@
-
+import { useEffect } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { About, Contact, Home, Projects } from './pages';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import SceneInit from './lib/SceneInit';
 
-const App = () => {
-return (
-    <main className="bg-slate-300/20">
-        <Router>
-            <Navbar/>
-            <Routes>
-                    <Route path="/" element={<Home/>} />
-                    <Route path="/about" element={<About/>} />
-                    <Route path="/projects" element={<Projects/>} />
-                    <Route path="/contact" element={<Contact/>} />
-                    
-                </Routes>
+function App() {
+    useEffect(() => {
+        const test = new SceneInit('myThreeJsCanvas');
+        test.initialize();
+        test.animate();
+        
+        const glftLoader = new GLTFLoader();
+        glftLoader.load('./src/pages/assets/building/scene.gltf', (gltfScene) => {
+            test.scene.add(gltfScene.scene);
+        });
+    }, []);
+
+    return (
+        <div className="container">
+            <Router>
+                <Navbar />
+                <main className="bg-slate-300/20">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/projects" element={<Projects />} />
+                        <Route path="/contact" element={<Contact />} />
+                    </Routes>
+                </main>
             </Router>
-            
-           
-    
-    </main>
-);
+            {window.location.pathname === '/' && <canvas id="myThreeJsCanvas" />} {/* Render canvas only on the main page */}
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,16 @@ import { NavLink } from "react-router-dom"
 const Navbar = () => {
     return ( 
        <header className="header">
-        <NavLink to="/" className="w-10 h-10 rounded-lg bg-slate-950 
+        <NavLink to="/" className="w-10 h-10 rounded-lg bg-white  
         items-center justify-center flex font-bold shadow-md">
         
         <p className= "blue-gradient_text"> DG</p>
         </NavLink>
         <nav className="flex text-lg gap-7 font-medium">
-        <NavLink to="/about" className = {({isActive}) => isActive? 'text-blue-500' : 'text-black'}>
+        <NavLink to="/about" className = {({isActive}) => isActive? 'text-blue-500' : 'text-red-700'}>
             About
         </NavLink>
-        <NavLink to="/projects" className = {({isActive}) => isActive? 'text-blue-500' : 'text-black'}>
+        <NavLink to="/projects" className = {({isActive}) => isActive? 'text-blue-500' : 'text-red-700'}>
             Projects
         </NavLink>
         
